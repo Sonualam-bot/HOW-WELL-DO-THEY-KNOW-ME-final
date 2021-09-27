f452244c219fadc3d697f0b10a13a6142510f809
Sonu Alam <61339832+Sonualam-bot@users.noreply.github.com>
2021-09-27 21:33:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var readLineSync = require('readline-sync');
+
+
+var userName = readLineSync.question("What's Your Name?");
+
+console.log("welcome "+ userName + " to Do YOU KNOW SONU?");
+
+// data of high score
+var highScores = [
+  {
+    name: "Monu",
+    score: 3,
+  },
+
+  {
+    name: "Gonu",
+    score: 2,
+  },
+]
+
+var score = 0;
+
+function play(question, answer){
+  var userAnswer = readLineSync.question(question);
+
+  if (userAnswer === answer){
+    console.log("You were right!");
+    score = score + 1;
+  }else{
+    console.log("You were wrong!");
+    score = score - 1;
+  }
+  console.log("current score: ", score);
+  console.log("-------------")
+}
+
+play("where do i live?", "Diyun");
+play("where do i study? ", "College");
+play("what do i play? ", "Football");
+play("what is favourite color?", "Blue");
+play("How old am i?", "21 years")
+
+
+
+// console.log("Your score is ", score);
+
+function showScores() {
+  console.log("YAY! You SCORED: ", score);
+
+  console.log("Check out the high scores, if you should be there ping me and I'll update it");
+
+  highScores.map(score => console.log(score.name, " : ", score.score))
+}
+
+showScores();
